47c327fc8b78d4afc8748b9c52e9633357d880db
recanman <29310982+recanman@users.noreply.github.com>
2025-07-02 16:26:02 -0700

    feat: add `VolumeIDLength` to SVOL structure, use buffered reader

--- pkg/pxi/chunks/svol/main.go
+++ pkg/pxi/chunks/svol/main.go
@@ -16,28 +16,30 @@ limitations under the License.
 package svol
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"io"
 
 	"github.com/PextraCloud/pxitool/pkg/pxi/chunk"
 	"github.com/PextraCloud/pxitool/pkg/pxi/constants/volumetype"
 )
 
 type Data struct {
-	VolumeType volumetype.VolumeType
-	VolumeID   string
-	Reserved   [4]byte // Reserved for future use, must be zeroed
-	VolumeData *io.Reader
+	VolumeType     volumetype.VolumeType
+	VolumeIDLength uint8
+	VolumeID       string
+	Reserved       [4]byte // Reserved for future use, must be zeroed
+	VolumeData     *bufio.Reader
 }
 
 type SVOL struct {
 	chunk.Chunk
-	VolumeDataReader *io.Reader // Reader for volume data
 }
 
 // Creates a new SVOL chunk with the specified parameters.
 func New(volumeType volumetype.VolumeType, volumeId string) *SVOL {
-	dataLen := 1 + len(volumeId) + 4 // 1 byte for volume type uint8, len(volumeId) bytes for volume ID, 4 reserved bytes
+	volumeIdLen := len(volumeId)
+	dataLen := 1 + 1 + volumeIdLen + 4 // 1 byte for volume type, 1 byte for volume ID length, len(volumeId) bytes for volume ID, 4 reserved bytes
 	c := &SVOL{
 		Chunk: chunk.Chunk{
 			Length:    uint64(dataLen),
@@ -47,12 +49,13 @@ func New(volumeType volumetype.VolumeType, volumeId string) *SVOL {
 	}
 
 	// Set volume type
-	c.Data[0] = byte(volumeType)
-	// Set volume ID
+	c.Data[0] = uint8(volumeType)
+	c.Data[1] = uint8(volumeIdLen)
+
 	volumeIdBytes := []byte(volumeId)
-	copy(c.Data[1:1+len(volumeIdBytes)], volumeIdBytes)
+	copy(c.Data[2:2+len(volumeIdBytes)], volumeIdBytes)
 	// Zero out reserved bytes
-	copy(c.Data[1+len(volumeIdBytes):1+len(volumeIdBytes)+4], []byte{})
+	copy(c.Data[2+len(volumeIdBytes):2+len(volumeIdBytes)+4], []byte{})
 
 	c.CRC32()
 	return c
@@ -64,27 +67,29 @@ func IncrementLength(c *SVOL, length uint64) {
 
 // TODO: use io.Reader
 func GetDataStruct(data []byte) (*Data, error) {
-	if len(data) < 5 {
+	if len(data) < 6 {
 		return nil, fmt.Errorf("data too short for SVOL chunk: %d bytes", len(data))
 	}
 
-	volumeType := volumetype.VolumeType(data[0])
-	volumeIdLen := len(data) - 5 // 1 byte for volume type, 4 reserved bytes
-	if volumeIdLen < 1 {
-		return nil, fmt.Errorf("invalid volume ID length: %d", volumeIdLen)
-	}
+	volumeType := data[0]
+	volumeIdLen := data[1]
 
-	volumeId := string(data[1 : 1+volumeIdLen-4])
+	volumeId := string(data[2 : 2+volumeIdLen])
 	reserved := [4]byte{}
-	copy(reserved[:], data[1+volumeIdLen-4:])
+	copy(reserved[:], data[2+volumeIdLen:])
 
 	if err := verifyReservedBytes(reserved); err != nil {
 		return nil, err
 	}
+
+	// 1 byte volume type, 1 byte volume ID length, volumeIdLen bytes for volume ID, 4 reserved bytes, 4 bytes CRC32 (zeroed in this case)
+	offset := 1 + 1 + volumeIdLen + 4 + 4
 	return &Data{
-		VolumeType: volumeType,
-		VolumeID:   volumeId,
-		Reserved:   reserved,
+		VolumeType:     volumetype.VolumeType(volumeType),
+		VolumeIDLength: volumeIdLen,
+		VolumeID:       volumeId,
+		Reserved:       reserved,
+		VolumeData:     bufio.NewReader(bytes.NewReader(data[offset:])),
 	}, nil
 }
 
